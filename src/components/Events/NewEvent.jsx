86eb4bbd430b7aa58256c816df07f9bc1661f775
Muhammad Hamza <mhdeveloper56@gmail.com>
2024-01-30 23:49:21 +0500

    Added New Files

---
+++ src/components/Events/NewEvent.jsx
@@ -0,0 +1,45 @@
+import { Link, useNavigate } from "react-router-dom";
+
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation } from "@tanstack/react-query";
+import { createNewEvent } from "../utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+
+export default function NewEvent() {
+  const navigate = useNavigate();
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
+
+  return (
+    <Modal onClose={() => navigate("../")}>
+      <EventForm onSubmit={handleSubmit}>
+        {isPending && <p>Submiting...</p>}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
+      </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="An error has occured"
+          message={
+            error.info?.message || "Couldn't create an event, Please Try again!"
+          }
+        />
+      )}
+    </Modal>
+  );
+}
